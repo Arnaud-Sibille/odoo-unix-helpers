805910032db3c25599a1a2aa121050e7902d68e4
Arnaud Sibille <arsi@odoo.com>
2024-04-14 19:15:07 +0200

    executing python in alias file

--- set_up.py
+++ set_up.py
@@ -70,7 +70,7 @@ def get_program_alias_name(program_path):
 
 def get_program_alias_command(program_path):
     program_name = get_program_alias_name(program_path)
-    return f"alias {program_name}={program_path}\n"
+    return f"alias {program_name}=python3 {program_path}\n"
 
 def get_programs():
     file_names = os.listdir(get_repo_directory())
